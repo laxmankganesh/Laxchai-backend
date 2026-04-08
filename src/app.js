458f463b77055feeff91173a9482e5a4a10c0d6f
laxmankganesh <laxmankganesh@gmail.com>
2026-04-08 18:37:52 +0530

    add register controller  and bug fixes

---
+++ src/app.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+app.use(bodyParser.json({limit: "16kb"}));
+app.use(bodyParser.urlencoded({limit: "16kb", extended: true}));
+
+// Routes
+app.get('/', (req, res) => {
+    res.json({message: "Server is running"});
+});
+
+export {app};
